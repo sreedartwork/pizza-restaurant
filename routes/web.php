fa8eb4110662d51972ad27c7f75a38169689f522
Sean Reed <sreedartwork@gmail.com>
2020-03-05 00:22:24 -0500

    added food categories page html and css

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,73 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Static Pages
+Route::get('/', 'StaticPagesController@home');
+Route::get('/menu', 'StaticPagesController@menu');
+Route::get('/menu/{slug}', 'StaticPagesController@singleMenu');
+Route::get('/about', 'StaticPagesController@about');
+Route::get('/reservations', 'StaticPagesController@reservations');
+Route::post('/reservations', 'StaticPagesController@saveReservation');
+Route::get('/reservations/thank-you', 'StaticPagesController@thankyou');
+Route::get('/contact', 'StaticPagesController@contact');
+Route::get('/offers', 'StaticPagesController@offers');
+Route::post('/offers', 'StaticPagesController@registerMember');
+Route::get('/offers/thank-you', 'StaticPagesController@thankYou');
 
-Route::get('/', function () {
-    return view('home');
+// Admin Dashboard
+Route::get('/admin', 'admin\AdminController@dashboard');
+
+// Admin Food Categories
+Route::get('/admin/food-categories', 'admin\FoodCategoriesController@index');
+Route::get('/admin/food-categories/create', 'admin\FoodCategoriesController@create');
+Route::post('/admin/food-categories', 'admin\FoodCategoriesController@store');
+Route::get('/admin/food-categories/{id}/edit', 'admin\FoodCategoriesController@edit');
+Route::put('/admin/food-categories/{id}', 'admin\FoodCategoriesController@update');
+Route::delete('/admin/food-categories/{id}/delete', 'admin\FoodCategoriesController@delete');
+
+// Admin Food Items
+Route::get('/admin/food-items', 'admin\FoodItemsController@index');
+Route::get('/admin/food-items/create', 'admin\FoodItemsController@create');
+Route::post('/admin/food-items', 'admin\FoodItemsController@store');
+Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
+Route::put('/admin/food-items/{id}', 'admin\FoodItemsController@update');
+Route::delete('/admin/food-items/{id}/delete', 'admin\FoodItemsController@delete');
+
+// Admin Users
+Route::get('/admin/users', 'admin\UsersController@index');
+Route::get('/admin/users/create', 'admin\UsersController@create');
+Route::post('/admin/users', 'admin\UsersController@store');
+Route::get('/admin/users/{id}/edit', 'admin\UsersController@edit');
+Route::put('/admin/users/{id}', 'admin\UsersController@update');
+Route::delete('/admin/users/{id}/delete', 'admin\UsersController@delete');
+
+// Admin Customers
+//Admin Members
+Route::get('/admin/members', 'admin\MemberController@index');
+Route::delete('/admin/members/{id}/delete', 'admin\MemberController@delete');
+
+//Admin Reservations
+Route::get('/admin/reservations', 'admin\ReservationController@index');
+Route::delete('/admin/reservations/{id}/delete', 'admin\ReservationsController@delete');
+
+//Admin Settings
+Route::get('/admin/settings/general', 'admin\SettingController@general');
+Route::put('/admin/settings/general', 'admin\SettingController@saveGeneral');
+Route::get('/admin/settings/seo', 'admin\SettingController@seo');
+Route::put('/admin/settings/seo', 'admin\SettingController@saveSeo');
+Route::get('/admin/settings/social', 'admin\SettingController@social');
+Route::put('/admin/settings/social', 'admin\SettingController@saveSocial');
+
+// Admin Authentication
+Route::get('/admin/register', function () {
+    return view('admin/register');
 });
-Route::get('/menu', function () {
-    return view('menu/index');
-});    
-Route::get('/menu/{slug}', function () {
-    return view('menu/single-menu');
+Route::get('/admin/login', function () {
+    return view('admin/login');
 });
-Route::get('/about', function () {
-    return view('pages/about');
-});
-Route::get('/waitlist', function () {
-    return view('pages/waitlist');
-});
-Route::get('/contact', function () {
-    return view('pages/contact');
-});
-Route::get('/giftcards', function () {
-    return view('pages/giftcards');
-});
-Route::get('/offers', function () {
-    return view('pages/offers');
-});
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
