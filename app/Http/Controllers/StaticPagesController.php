3c70575ddae01ba195cd5f5240e09bbd79457a88
Sean Reed <sreedartwork@gmail.com>
2020-03-05 13:39:42 -0500

    Created food items admin pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Member;
+use App\Reservation;
+
+class StaticPagesController extends Controller
+{
+    public function home(){
+        return view('home');
+    }
+    public function about(){
+        return view('pages/about');
+    }
+    public function reservations(){
+        return view('pages/reservations');
+    }
+    public function saveReservation(){
+        request()->validate([
+            'fname' => ['required', 'string'],
+            'lname' => ['required', 'string'],
+            'email' => ['required', 'string'],
+            'phone_number' => ['required', 'string'],
+            'guests_total' => ['required', 'string'],
+            'time' => ['required', 'string'],
+        ]);
+        $reservation = new Reservation();
+        $reservation->fname = request('fname');
+        $reservation->lname = request('lname');
+        $reservation->email = request('email');
+        $reservation->phone_number = request('phone_number');
+        $reservation->guests_total = request('guests_total');
+        $reservation->time = request('time');
+        $reservation->save();
+
+       
+        return redirect('/reservations/thank-you');
+    }
+    public function contact(){
+        return view('pages/contact');
+    }
+    public function offers(){
+        return view('pages/offers');
+    }
+    public function registerMember(){
+        request()->validate([
+            'fname' => ['required', 'string'],
+            'lname' => ['required', 'string'],
+            'email' => ['required', 'string'],
+            'phone_number' => ['required', 'string'],
+        ]);
+        $member = new Member();
+        $member->fname = request('fname');
+        $member->lname = request('lname');
+        $member->email = request('email');
+        $member->phone_number = request('phone_number');
+        $member->save();
+
+       
+        return redirect('/offers/thank-you');
+    }
+    public function thankYou(){
+        return view('pages/thank-you');
+    }
+    public function menu(){
+        return view('menu/index');
+    }
+    public function singleMenu(){
+        return view('menu/single-menu');
+    }
+}
+
+
