66767ff1a85f093edee073330bf460df9676aa94
Sean Reed <sreedartwork@gmail.com>
2020-03-08 01:09:39 -0500

    Added show all users and pagination

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,8 +17,8 @@ class UsersTableSeeder extends Seeder
             'lname' => 'Smith',
             'email' => 'billy@gmail.com',
             'password' => Hash::make('12345678'),
-            // 'updated_at' => Carbon::now(),
-            // 'created_at' => Carbon::now()
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
         ]);
         
         DB::table('users')->insert([
@@ -26,8 +26,8 @@ class UsersTableSeeder extends Seeder
             'lname' => 'Johnson',
             'email' => 'cindy@gmail.com',
             'password' => Hash::make('12345678'),
-        //     'updated_at' => Carbon::now(),
-        //     'created_at' => Carbon::now()
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
         ]);
     }
 }
