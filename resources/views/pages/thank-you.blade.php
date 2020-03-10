fd7f2d183a07adb61f2884c1eb5c950dd92cf73e
Sean Reed <sreedartwork@gmail.com>
2020-03-10 12:37:36 -0400

    created new member from front page

---
+++ resources/views/pages/thank-you.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="offers-page">
+
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 thank-you">
+                    <h1>Thank You</h1>
+                    <p>We will get back to you.</p>
+
+                </div>
+            </div>
+    </div> 
+@endsection
