f4f122157a24484b06909cafe891fd6b06d08cc4
Sean Reed <sreedartwork@gmail.com>
2020-03-19 02:04:03 -0400

    Added Dashboards for Admin and Employees

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
 
 class RolesTableSeeder extends Seeder
 {
@@ -30,5 +32,13 @@ class RolesTableSeeder extends Seeder
             'role_id' => 2,
             'user_id' => 2,
         ]);
+        
+        $faker = Faker::create();
+        foreach(range(3,20) as $index){
+            DB::table('role_user')->insert([
+                'role_id' => rand(1,2),
+                'user_id' => $index,
+            ]);
+        }
     }
 }
